4ff9e6981f6bd89fd892c14a78dff8bd60090215
Laili29RPL <75743575+Laili29RPL@users.noreply.github.com>
2021-12-02 08:59:34 +0700

    Add files via upload

---
+++ rent_car/login-process.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include "connection.php";
+#session->tempat penyimpanan data di sisi server
+#yang dapat diakses secara global pada halaman web yang 
+#membutuhkan 
+
+if (isset($_POST["login"])) {
+    #menampung data username dan password
+    $username = $_POST["username"];
+    $password = sha1($_POST["password"]);
+
+    #ambil data karyawan sesuai password dan username yang diberikan 
+    $sql = "select * from karyawan where 
+    username='$username' and password='$password'";
+    $hasil = mysqli_query($connect, $sql);
+}
+
+#cek hasil query
+#mysqli_num_rows-> cek jumlah bais hasil query
+if (mysqli_num_rows($hasil) > 0) {
+    # login berhasil
+    #data disimpan ke dalam session
+    $karyawan = mysqli_fetch_array($hasil);
+    $_SESSION["karyawan"] = $karyawan;
+    header("location:form-sewa.php");
+}else {
+    #login gagal
+    header("location:login.php");
+}
+?>
---
+++ rent_car/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Login Karyawan</title>
+</head>
+<body>
+    <div class="container">
+
+    </div>
+    <div class="card col-lg-6 mx-auto">
+        <div class="card-header bg-info">
+            <h4 class="text-white">
+                Login Karyawan
+            </h4>
+        </div>
+        <div class="card-body">
+            <form action="login-process.php" method="post">
+                Username
+                <input type="text" name="username" 
+                class="form-control mb-2" required>
+            
+                Password
+                <input type="password" name="password" 
+                class="form-control mb-2" required>
+
+                <button type="submit" name="login"
+                class="btn btn-info btn-block">
+                    Log In
+                </button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
